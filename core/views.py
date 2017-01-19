eb6818cf3a08a2b0f6f8ee22c7e601ba19c11d43
Nate Cunningham <nrcunningham@bremenhospital.net>
2017-01-19 16:27:58 -0500

    fixed stuff

--- core/views.py
+++ core/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Department
-# Create your views here.
+
 def index(request):
 	department_list = Department.objects.all()
-	output = ', '.join([dept.name for dept in department_list])
-	return HttpResponse(output)
+	context = {'department_list': department_list}
+
+	return render(request, 'core/index.html', context)
+
+def department(request, pk):
+	return HttpResponse("Hello From Department")
